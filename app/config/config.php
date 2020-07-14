e920bb256326de4daa1ad7e17805e0a259002134
Carol <carol@pop-os.localdomain>
2020-07-14 19:44:43 -0300

    quase tudo gostoso

---
+++ app/config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+define('BASE', '/quase-tudo-gostoso/');
+define('HOST', 'http://localhost/quase-tudo-gostoso/');
+
+define('DATE_TIME', 'd/m/Y H:i:s');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '1234');
+define('DB_NAME', 'qt_gostoso');
+
+$router = [
+    /* VIEW */
+    'home' => 'HomeController@index',
+    'nova' => 'ReceitaController@nova',
+    'editar' => 'ReceitaController@editar',
+    'ver' => 'ReceitaController@ver',
+    'busca' => 'ReceitaController@busca',
+    /* INTERNAL */
+    'delete' => 'ReceitaController@delete',
+    'insert' => 'ReceitaController@insert',
+    'update' => 'ReceitaController@update'
+];
